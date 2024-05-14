ee0f2a9f78af3ff142604393428d23f39897af0c
Eline <163290231+ElineZ90@users.noreply.github.com>
2024-05-14 21:27:00 +0200

    day 2

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
@@ -45,5 +47,79 @@ public class Main {
         System.out.println("Length is: " + length);
         System.out.println("Width is: " + width);
         System.out.println("Area of the rectangle is: " + area);
+
+        // Booleans
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);
+        System.out.println(isFishTasty);
+
+        // Strings
+        String greeting = "Hello world";
+        System.out.println(greeting);
+
+        // Create variables of different data types
+        int items = 50;
+        float costPerItem = 9.99f;
+        float totalCost = items * costPerItem;
+        char currency = '$';
+
+        // Print variables
+        System.out.println("Number of items: " + items);
+        System.out.println("Cost per item: " + costPerItem + currency);
+        System.out.println("Total cost = " + totalCost + currency);
+
+        // Strings continued
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        System.out.println("The alphabet has a lot of letters: " + alphabet.length() + " in total");
+        System.out.println(alphabet.toUpperCase());
+        System.out.println(alphabet.indexOf("q"));
+        System.out.println(greeting + " " + studentName);
+        System.out.println(firstName.concat(lastName));
+
+        /* For special symbols such as singe quotes, double quotes and backslash use a backslash before it to escape.
+        Also the \n (new line) \r (return) \t (tab) \b (backspace) \f (form feed)  are used. */
+
+        // Maths
+
+        Math.max(5, 10);
+        System.out.println(Math.min(10, 20));
+
+        // Conditional testing
+        /* if (condition1) {
+        // block of code to be executed if condition1 is true
+        } else if (condition2) {
+         // block of code to be executed if the condition1 is false and condition2 is true
+        } else {
+        // block of code to be executed if the condition1 is false and condition2 is false
+        }
+        */ 
+
+        int myAge = 32;
+        int votingAge = 18;
+
+        if (myAge >= votingAge) {
+            System.out.println("I can vote");
+        } else {
+            System.out.println("Cannot yet vote");
+        }
+
+        // Ternary operator for if else with ? and : 
+        int time = 20;
+        String result = (time < 18) ? "Good day." : "Good evening.";
+        System.out.println(result);
+
+        // Next up: Java Switch w3wchools.com/java/java_switch.asp
+
+
+
+
+
+
+
+
+
+
+
     }
 }
